e8ea30e0eb58e84f3d91678841f867a258e74a50
ShaChambers <shaheedchambers@gmail.com>
2024-03-12 18:35:11 -0400

    switched values to create a galaxy look

--- src/script.js
+++ src/script.js
@@ -24,8 +24,17 @@ const textureLoader = new THREE.TextureLoader()
  */
 
 //Geometry
-const particlesGeometry = new THREE.SphereGeometry(1, 32, 32)
+const particlesGeometry = new THREE.BufferGeometry
+const count = 500
 
+const positions = new Float32Array(count * 2)
+
+for (let i = 0; i < count * 3; i++) {
+    positions[i] = (Math.random() - 0.5) * 10
+    
+}
+
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
 //Material
 const particlesMaterial = new THREE.PointsMaterial()
     particlesMaterial.size = 0.02,
